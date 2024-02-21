39e7c640a0748f0a2c2b0528727c24201fcd1cf2
Rajvardhan135 <rajvardhansinghchib@gmail.com>
2024-02-21 22:31:08 +0530

    3d-website-clone complete

---
+++ src/components/Hero.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+import { styles } from '../styles'
+import { ComputersCanvas } from './canvas'
+
+const Hero = () => {
+  return (
+    <section className='relative w-full h-screen py-16 mx-auto mb-4'>
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+        <div className='flex flex-col items-center justify-center mt-5'>
+          <div className='w-5 h-5 rounded-full bg-[#915eff]' />
+          <div className='w-1 h-40 sm:h-80 violet-gradient' />
+        </div>
+        <div className=''>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff]'>Rajvardhan</span></h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I develop 3D visuals, user Interfaces <br className='hidden sm:block' /> and web applications
+          </p>
+        </div>
+      </div>
+      <ComputersCanvas />
+      <div className='absolute flex items-center justify-center w-full xs:bottom-8 bottom-1'>
+        <a href='#about'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.div
+              animate={{
+                y: [0, 24, 0]
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: 'loop'
+              }}
+              className='w-3 h-3 mb-1 rounded-full bg-secondary'
+            />
+          </div>
+        </a>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
